928acf6517eaa74f567ba62db0922c77c5c0387c
deepika <adeepika1991@gmail.com>
2025-05-19 21:36:35 -0400

    SignIn SignOut forms added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
@@ -8,7 +9,7 @@ const monaSans = Mona_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "MockGPT",
+  title: "InterviewGenius",
   description: "An AI-Powered Mock Interview Platform",
 };
 
@@ -20,8 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${monaSans.className} antialiased`}
+        className={`${monaSans.className} antialiased pattern`}
       >
+        <Toaster/>
         {children}
       </body>
     </html>
